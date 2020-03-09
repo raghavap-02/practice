02af32e6e71f03a7eefdbb976ca2c0e5d3838012
Raghavendra <61932739+raghavap-02@users.noreply.github.com>
2020-03-09 13:40:11 +0530

    Update App.java
    update sysout

--- src/main/java/ToolsQA/App.java
+++ src/main/java/ToolsQA/App.java
@@ -4,10 +4,12 @@ package ToolsQA;
  * Hello world!
  *
  */
+
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World robo here!" );
     }
 }
